27f7b33fb3ce21c83abebb25d85ba9c96e7e0626
Manpreet <manpreets95828@gmail.com>
2025-05-28 22:52:47 +0530

    JWT verify and JWT sign

--- Authentication/index.js
+++ Authentication/index.js
@@ -1,25 +1,60 @@
 const express = require("express");
 const jwt = require("jsonwebtoken");
-
+const JWT_SECRET = "Manpreet123";
 const app = express();
 
 const users = [];
+app.use(express.json());
 
 app.post("/signup", function(req, res){
-  const username = req.body.username;
-  const password = req.body.password;
-  users.push({username, password});
+  const username = req.body.username
+  const password = req.body.password
+  users.push({
+    username: username, 
+    password: password
+  })
   // we should check if the user with same username already exists 
-  res.json({message: "You are signed in!"});
-
+  res.json({
+    message: "You are signed in!"
+  })
 })
 
 app.post("/signin", function(req, res){
-
+  const username = req.body.username;
+  const password = req.body.password;
+  const foundUser = users.find(user => user.username === username && user.password === password);
+  if(!foundUser){
+    res.json({message: "Credentials incorrect"});
+    return;
+  }else{
+    const token = jwt.sign({
+      username
+    }, JWT_SECRET);
+    res.json({
+      token: token
+    })
+  }
 })
 
 app.get("/me", function(req, res){
+  const token = req.headers.token;
 
+  const decodeData = jwt.verify(token, JWT_SECRET);
+
+  if(decodeData.username){
+    let foundUser = null;
+    for(let i = 0; i<users.length;i++){
+      if(users[i].username === decodeData.username){
+        foundUser = users[i];
+      }
+    }
+
+
+    res.json({
+      username: foundUser.username,
+      password: foundUser.password
+    })
+  }
 })
 
-const port(3000);
+app.listen(3000);
